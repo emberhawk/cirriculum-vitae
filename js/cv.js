293afc2063e1f58458422c2dcd3db26b2df7fc52
TUFFY <emberhawk@tuffy-04.Fatcontroller>
2021-03-25 09:03:06 +0000

    updated shaders on blog

---
+++ js/cv.js
@@ -0,0 +1,39 @@
+function myIQDrop(){
+  var x = document.getElementById('iq');
+
+  if (x.style.display === "none"){
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myMBTIDrop(){
+  var x = document.getElementById('mbti');
+
+  if (x.style.display === "none"){
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myEnnDrop(){
+  var x = document.getElementById('enn');
+
+  if (x.style.display === "none"){
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myHollDrop(){
+  var x = document.getElementById('holl');
+  
+  if (x.style.display === "none"){
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
